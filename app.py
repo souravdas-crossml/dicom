17fec7de9652bf8f0eef337b6af8d95d104ae87a
Sourav Das <sourav.das@crossml.com>
2024-04-08 19:51:37 +0530

    Updated Dicom Saver

--- app.py
+++ app.py
@@ -2,16 +2,33 @@
 
 """
 
+import os
 from fastapi import FastAPI, File, UploadFile
 
+from utils.dicom_saver import DicomSaver
+
 app = FastAPI()
+
+dicom_saver = DicomSaver(
+    s3_bucket=os.getenv("S3_BUCKET"),
+    region_name=os.getenv("REGION_NAME"),
+    access_key_id=os.getenv("ACCESS_KEY_ID"),
+    secret_access_key=os.getenv("SECRET_ACCESS_KEY"),
+)
+
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
     # Read the contents of the file
     contents = await file.read()
     # Return file details
-    return {"filename": file.filename, "content_type": file.content_type, "size": len(contents)}
+    
+    dicom_saver.process_and_save(
+        dicom_bytes=contents,
+        filename=file.filename,
+    )
+    
+    return {"status": 200}
 
 
 
